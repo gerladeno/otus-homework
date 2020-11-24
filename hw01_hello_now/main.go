1793d01bc04d60765ab83bed7afa208f28fbdfdd
pavel <gerladeno@gmail.com>
2020-11-25 00:11:09 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const NTPServer = "ntp3.stratum2.ru"
+const timeFmt = "2006-01-02 03:04:05 -0700 MST"
+
 func main() {
-	// Place your code here
+	currentTime := time.Now().UTC()
+	exactTimeNtp, err := ntp.Time(NTPServer)
+	if err != nil {
+		panic(err)
+	}
+	exactTime := exactTimeNtp.UTC()
+	fmt.Printf("current time: %s\nexact time: %s\n", currentTime.Format(timeFmt), exactTime.Format(timeFmt))
 }
