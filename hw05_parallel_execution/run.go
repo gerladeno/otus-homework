a7ba6efb81faa8f524961385137274f5e6192d21
pavel <gerladeno@gmail.com>
2021-01-15 21:50:57 +0300

    wip, doesn't work

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,17 +2,11 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
-	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
-
-type safeCnt struct {
-	cnt int
-	mx  sync.Mutex
-}
 
 func worker(jobs <-chan Task, result chan<- struct{}) {
 	for i := range jobs {
@@ -24,47 +18,22 @@ func worker(jobs <-chan Task, result chan<- struct{}) {
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	wg := sync.WaitGroup{}
-	errs := safeCnt{}
-	tasksChan := make(chan Task)
-	queue := make(chan struct{}, N)
+	tasksChan := make(chan Task, len(tasks))
 	errChan := make(chan struct{})
-	allDone := make(chan struct{})
 	go func() {
-		for _, task := range tasks {
-			tasksChan<- task
+		tasksLoop: for _, task := range tasks {
 			select {
+			case tasksChan <- task:
 			case <-errChan:
 				break
 			}
+
 		}
 	}()
-	for i := 0; i < N; i++ {
-		queue <- struct{}{}
-		go func() {
-			wg.Add(1)
-			defer wg.Done()
-			if err := task(); err != nil {
-				errs.mx.Lock()
-				errs.cnt++
-				if errs.cnt >= M {
-					errChan <- struct{}{}
-					return
-				}
-				errs.mx.Unlock()
-			}
-			<-queue
-		}()
-	}
-
 	go func() {
-		wg.Wait()
-		allDone <- struct{}{}
+		for i := 0; i < N; i++ {
+			go worker(tasksChan, errChan)
+		}
 	}()
-	select {
-	case <-errChan:
-		return ErrErrorsLimitExceeded
-	case <-allDone:
-		return nil
-	}
+	return nil
 }
