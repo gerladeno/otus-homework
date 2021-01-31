2ca768d13400e5bb58456ae8d8a4208d0eaa7b5d
pavel <gerladeno@gmail.com>
2021-01-31 15:39:03 +0300

    wip

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,4 +1,8 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
+import (
+	"log"
+	"sync"
+)
 
 type (
 	In  = <-chan interface{}
@@ -9,26 +13,50 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	//var input Bi
-	input := make(Bi)
-	go func() {
-		//input = make(Bi)
-		defer close(input)
-		for item := range in{
+	transmitter := func(in Bi, done In, out Out) {
+		for {
 			select {
 			case <-done:
+				close(in)
 				return
-			case input <- item:
+			default:
+				select {
+				case <-done:
+					close(in)
+					return
+				case tmp, ok := <-out:
+					if ok {
+						in <- tmp
+					} else {
+						close(in)
+						return
+					}
+				}
 			}
 		}
-	}()
-	var out Out
-	for _, stage := range stages {
-		if out == nil {
-			out = stage(input)
-		} else {
-			out = stage(out)
-		}
 	}
-	return out
+	var wg = sync.WaitGroup{}
+	for _, stage := range stages {
+		wg.Add(1)
+		log.Print("main +1")
+		stage := stage
+		go func() {
+			defer wg.Done()
+			defer log.Printf("main -1")
+			input := make(Bi)
+			wg.Add(1)
+			log.Printf("inner +1")
+			go func() {
+				defer wg.Done()
+				defer log.Printf("inner -1")
+				transmitter(input, done, in)
+			}()
+			in = stage(input)
+		}()
+		//input := make(Bi)
+		//go transmitter(input, done, in)
+		//in = stage(input)
+	}
+	wg.Wait()
+	return in
 }
