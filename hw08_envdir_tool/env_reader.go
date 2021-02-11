8e4f178fb94549a96b5598c0cc0ec570f13c2ebf
pavel <gerladeno@gmail.com>
2021-02-12 01:24:22 +0300

    hw done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +18,47 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	contents, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	envs := make(Environment)
+	for _, file := range contents {
+		if file.Mode().IsRegular() {
+			name := file.Name()
+			if strings.Contains(name, "=") {
+				log.Printf("filename \"%s\" contains \"=\", skipped...", name)
+			}
+			value, err := readEnv(filepath.Join(dir, name))
+			if err != nil {
+				log.Printf("can't read file \"%s\", %s, skipped...", name, err.Error())
+				continue
+			}
+			envs[name] = *value
+		}
+	}
+	return envs, nil
+}
+
+func readEnv(file string) (*EnvValue, error) {
+	valueBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(valueBytes) == 0 {
+		return &EnvValue{"", true}, nil
+	}
+
+	str := string(valueBytes)
+	if strings.Contains(str, "\n") {
+		str = strings.Split(str, "\n")[0]
+	}
+	str = strings.ReplaceAll(str, "\x00", "\n")
+	value := strings.TrimRight(strings.TrimRight(str, " "), "\t")
+	env := EnvValue{
+		Value:      value,
+		NeedRemove: false,
+	}
+	return &env, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
 
-func TestReadDir(t *testing.T) {
-	// Place your code here
+func TestReadTestDir(t *testing.T) {
+	testdata := make(Environment)
+	testdata["BAR"] = EnvValue{"bar", false}
+	testdata["EMPTY"] = EnvValue{"", false}
+	testdata["FOO"] = EnvValue{"   foo\nwith new line", false}
+	testdata["HELLO"] = EnvValue{"\"hello\"", false}
+	testdata["UNSET"] = EnvValue{"", true}
+	result, err := ReadDir("testdata/env")
+	require.Nil(t, err)
+	require.Equal(t, result, testdata)
+}
+
+func TestEmptyDir(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "temp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := os.Remove(tmp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	testdata := make(Environment)
+	result, err := ReadDir(tmp)
+	require.Nil(t, err)
+	require.Equal(t, result, testdata)
 }
