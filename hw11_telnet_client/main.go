b3d0803a74484940f762add4b9af460e7f7bcc8a
pavel <gerladeno@gmail.com>
2021-03-17 23:17:46 +0300

    hw done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,64 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeoutFlag := pflag.String("timeout", "30s", "specify duration, default - 30s")
+	pflag.Parse()
+	timeout, err := time.ParseDuration(*timeoutFlag)
+	if err != nil {
+		log.Fatal("invalid timeout")
+	}
+	args := pflag.CommandLine.Args()
+	if len(args) < 2 {
+		log.Fatal("too few arguments")
+	}
+
+	telnet := NewTelnetClient(net.JoinHostPort(args[0], args[1]), timeout, os.Stdin, os.Stdout)
+
+	if err := startTelnet(telnet); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startTelnet(telnet TelnetClient) error {
+	err := telnet.Connect()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go worker(telnet.Receive, cancel)
+	go worker(telnet.Send, cancel)
+
+	signalChan := make(chan os.Signal)
+
+	go func() {
+		signal.Notify(signalChan, syscall.SIGINT)
+	}()
+	select {
+	case <-signalChan:
+		cancel()
+		signal.Stop(signalChan)
+	case <-ctx.Done():
+		close(signalChan)
+	}
+	return nil
+}
+
+func worker(handler func() error, cancel context.CancelFunc) {
+	if err := handler(); err != nil {
+		cancel()
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +13,43 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type Client struct {
+	address    string
+	timeout    time.Duration
+	in         io.ReadCloser
+	out        io.Writer
+	connection net.Conn
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Connect() error {
+	var err error
+	c.connection, err = net.DialTimeout("tcp", c.address, c.timeout)
+	return err
+}
+
+func (c *Client) Close() error {
+	err := c.connection.Close()
+	return err
+}
+
+func (c *Client) Send() error {
+	return c.readWrite(c.in, c.connection)
+}
+
+func (c *Client) Receive() error {
+	return c.readWrite(c.connection, c.out)
+}
+
+func (c *Client) readWrite(rd io.Reader, wr io.Writer) error {
+	_, err := io.Copy(wr, rd)
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
